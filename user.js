e8b304794547e3c78311724fcd35c3a71c5a63dc
Florin Tobler <florin.tobler@hotmail.com>
2018-03-17 11:18:06 +0100

    first commit

---
+++ user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         XKCD Keybord Browser
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Use 'A' or 'J' to go back; 'D' or 'L' to go forward.
+// @author       Florin Tobler
+// @match        https://xkcd.com/*/
+// @grant        CC-0-3
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    //on keypress event
+    document.onkeypress = function(event) {
+        //console.log(event);
+        var key = event.key;
+        if (key == 'a' || key == 'j') {
+            navigatePage(-1);
+        } else if (key == 'd' || key == 'l') {
+            navigatePage(+1);
+        }
+    };
+
+    //function to navigate forward or backward
+    function navigatePage(increment) {
+        var url = location.href.split('/');
+        var nr = parseInt(url[3]);
+        nr += increment;
+        url[3] = nr + "";
+        console.log(url);
+        location.href = url.join("/");
+    }
+})();
